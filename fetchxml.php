b497b1f932cf61c21302fbcb12239a068a9d5f11
Brian <bpchiv@gmail.com>
2015-04-18 13:43:01 -0400

    Fixed fetchxml.php
    Login issue and query issues fixed in fetchxml.php

--- fetchxml.php
+++ fetchxml.php
@@ -5,22 +5,23 @@ $user = $_GET['user'];
 $pass = $_GET['pw'];
 
 //Login check
-if(login($user,$pass) == false){
+if(login($user,$pass) != 'True'){
 	echo '<game status="no" msg="Login Fail" />';
 	exit;
 }
-
+$pdo = pdo_connect();
+$userQ= $pdo->quote($user);
 //Fetch game xml
-$query = "SELECT Xml FROM FlockingGame WHERE (Player1 ='$user' OR Player2='$user'";
+$query = "SELECT Xml FROM FlockingGame WHERE Player1 =$userQ OR Player2=$userQ";
 $pdo = pdo_connect();
 $result = $pdo->query($query);
-//If it exists, give it to them
-if($row = $result->fetch()){
-	echo $row['Xml'];
+//If game doesn't exist, throw error
+if($result == false){
+	echo '<game status="no" msg="Game not found" />';
 	exit;
 }
-//Otherwise, return an error
 else{
-	echo '<game status="no" msg="Game not found" />';
+	$row = $result->fetch();
+	echo $row['Xml'];
 	exit;
 }
